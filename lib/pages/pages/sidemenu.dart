49a1c1949781901b91f251d88df667948d1ff6a4
preethi <preethi@xmedia.in>
2023-12-27 18:32:54 +0530

    cidemenu

--- lib/pages/pages/sidemenu.dart
+++ lib/pages/pages/sidemenu.dart
@@ -9,25 +9,108 @@ class Sidemenu extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('assets/user.png'),
+                  backgroundColor: Colors.transparent,
+                  // Placeholder image for demonstration (replace with your image)
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Username',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 8),
+              ],
             ),
           ),
           ListTile(
-            title: Text('Item 1'),
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child: Image.asset('assets/dashboard.png', width: 24, height: 24),
+            ),
+            title: Text('Dashboard'),
             onTap: () {
               // Add functionality here when item 1 is tapped
             },
           ),
           ListTile(
-            title: Text('Item 2'),
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child: Image.asset('assets/search.png', width: 24, height: 24),
+            ),
+            title: Text('Search'),
+            onTap: () {
+              // Add functionality here when item 2 is tapped
+            },
+          ),
+          ListTile(
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child: Image.asset('assets/user.png', width: 24, height: 24),
+            ),
+            title: Text('My Profile'),
+            onTap: () {
+              // Add functionality here when item 2 is tapped
+            },
+          ),
+          ListTile(
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child: Image.asset('assets/settings.png', width: 24, height: 24),
+            ),
+            title: Text('Settings'),
+            onTap: () {
+              // Add functionality here when item 2 is tapped
+            },
+          ),
+          ListTile(
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child:
+                  Image.asset('assets/exclamation.png', width: 24, height: 24),
+            ),
+            title: Text('Raise Major Issues'),
+            onTap: () {
+              // Add functionality here when item 2 is tapped
+            },
+          ),
+          ListTile(
+            leading: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFFff0068), // Convert hexadecimal to Color
+
+                BlendMode.srcIn,
+              ),
+              child: Image.asset('assets/review.png', width: 24, height: 24),
+            ),
+            title: Text('App Feedback'),
             onTap: () {
               // Add functionality here when item 2 is tapped
             },
